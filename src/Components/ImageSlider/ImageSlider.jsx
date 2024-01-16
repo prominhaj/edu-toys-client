cea8b90ce6b2abe21703122a849790faa1ff6bf1
Md Minhaj <parsonal322532@gmail.com>
2024-01-17 05:40:08 +0600

    Change By ImageSlider Section Auto Play Add And Speed Add

--- src/Components/ImageSlider/ImageSlider.jsx
+++ src/Components/ImageSlider/ImageSlider.jsx
@@ -7,15 +7,19 @@ import ImageSliderItem from "../ImageSliderItem/ImageSliderItem";
 const settings = {
   dots: true,
   infinite: true,
+  speed: 1000,
   slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToScroll: 2,
   initialSlide: 0,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
@@ -24,7 +28,7 @@ const settings = {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         initialSlide: 2,
       },
     },
